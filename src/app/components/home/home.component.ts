340e67a446870068715a58044e25315d789d3a37
ynfleozin <leonardoalvarengam@gmail.com>
2025-02-12 21:22:24 -0300

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, NgOptimizedImage],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
