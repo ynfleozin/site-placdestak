8300a441241d72c3b5c6cdc56d4053c348ace343
ynfleozin <leonardoalvarengam@gmail.com>
2025-03-09 15:34:09 -0300

    add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: "",
-    component: HomeComponent
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "**",
+    title: 'Página não encontrada',
+    component: NotFoundComponent
   }
 ];
