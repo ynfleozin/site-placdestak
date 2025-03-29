fdfb71ee63a3565b4245f21de74b8a95bb0158c6
ynfleozin <leonardoalvarengam@gmail.com>
2025-03-29 12:59:22 -0300

    feat: add content and styles

--- src/app/components/company/company.component.ts
+++ src/app/components/company/company.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-company',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './company.component.html',
   styleUrl: './company.component.scss'
 })
