fbcdb3dae0c61fff5d6a4f218cd13f220cfd1238
ynfleozin <leonardoalvarengam@gmail.com>
2025-04-06 13:07:02 -0300

    add content and budget form

--- src/app/components/budget/budget.component.ts
+++ src/app/components/budget/budget.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 
@@ -10,5 +11,21 @@ import { FooterComponent } from "../footer/footer.component";
   styleUrl: './budget.component.scss'
 })
 export class BudgetComponent {
+  budgetForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+  ){
+    this.budgetForm = this.fb.group({
+      name: [''],
+      email: [''],
+      phone: [''],
+      message: [''],
+    })
+  }
+
+
+  setValue(){
+    this.budgetForm.setValue({})
+  }
 }
