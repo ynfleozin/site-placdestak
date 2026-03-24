510f0006f62b94aa09261dee42a579bebd791139
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-23 21:38:52 -0300

    feat: create portfolio modal component

---
+++ src/app/shared/components/portfolio-modal/portfolio-modal.component.ts
@@ -0,0 +1,40 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-portfolio-modal',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
+  templateUrl: './portfolio-modal.component.html',
+  styleUrl: './portfolio-modal.component.scss'
+})
+export class PortfolioModalComponent {
+  currentIndex = 0;
+
+  constructor(
+    public dialogRef: MatDialogRef<PortfolioModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {title: string; images: string[]}
+  ){}
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  previousImage(): void {
+    if(this.currentIndex > 0){
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.data.images.length - 1;
+    }
+  }
+
+  nextImage(): void {
+    if(this.currentIndex < this.data.images.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+}
