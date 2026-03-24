4ac45f1c8159ac308b14ff2232a158b060be1b57
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-24 20:20:31 -0300

    feat: add NgZone to improve performance

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,11 +5,17 @@ import {
   OnDestroy,
   OnInit,
   PLATFORM_ID,
+  NgZone,
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { NgOptimizedImage } from '@angular/common';
-import { PortfolioCardComponent } from "../../components/portfolio-card/portfolio-card.component";
-import { FEATURES, HEADLINE_TEXT, PORTFOLIO_IMGS, SERVICES } from '../../data/home-data';
+import {
+  FEATURES,
+  HEADLINE_TEXT,
+  PORTFOLIO_IMGS,
+  SERVICES,
+} from '../../data/home-data';
+import { PortfolioCardComponent } from '../../components/portfolio-card/portfolio-card.component';
 
 @Component({
   selector: 'app-home',
@@ -28,16 +34,23 @@ export class HomeComponent implements OnInit, OnDestroy {
   headlineIndex = 0;
   private intervalId!: ReturnType<typeof setInterval>;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private ngZone: NgZone,
+  ) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       this.isMobile = window.innerWidth <= 768;
 
-      this.intervalId = setInterval(() => {
-        this.headlineIndex =
-          (this.headlineIndex + 1) % this.headlineText.length;
-      }, 10000);
+      this.ngZone.runOutsideAngular(() => {
+        this.intervalId = setInterval(() => {
+          this.ngZone.run(() => {
+            this.headlineIndex =
+              (this.headlineIndex + 1) % this.headlineText.length;
+          });
+        }, 8000);
+      });
     }
   }
 
