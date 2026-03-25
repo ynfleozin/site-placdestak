e6d7a0faf5d8cc2f69acc141d213794e09b38b01
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-25 15:09:51 -0300

    feat: redesign footer structure and update style

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
