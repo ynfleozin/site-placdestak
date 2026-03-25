39c6e22613cfc4129bb8e95cfbe45d721e902b4e
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-25 14:42:36 -0300

    feat: redesign page structure and update tyle

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
