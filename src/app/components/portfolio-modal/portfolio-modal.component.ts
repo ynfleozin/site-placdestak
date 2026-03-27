38f120d022f8bbf9a0d8682ac774cbbcca6a53ba
ynfleozin <leonardoalvarengam@gmail.com>
2026-03-26 22:21:16 -0300

    feat: redesign portfolio modal and update style

--- src/app/components/portfolio-modal/portfolio-modal.component.ts
+++ src/app/components/portfolio-modal/portfolio-modal.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -8,22 +12,22 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [MatDialogModule, MatButtonModule, MatIconModule],
   templateUrl: './portfolio-modal.component.html',
-  styleUrl: './portfolio-modal.component.scss'
+  styleUrl: './portfolio-modal.component.scss',
 })
 export class PortfolioModalComponent {
   currentIndex = 0;
 
   constructor(
     public dialogRef: MatDialogRef<PortfolioModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {title: string; images: string[]}
-  ){}
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; images: string[] },
+  ) {}
 
   close(): void {
     this.dialogRef.close();
   }
 
   previousImage(): void {
-    if(this.currentIndex > 0){
+    if (this.currentIndex > 0) {
       this.currentIndex--;
     } else {
       this.currentIndex = this.data.images.length - 1;
@@ -31,10 +35,14 @@ export class PortfolioModalComponent {
   }
 
   nextImage(): void {
-    if(this.currentIndex < this.data.images.length - 1) {
+    if (this.currentIndex < this.data.images.length - 1) {
       this.currentIndex++;
     } else {
       this.currentIndex = 0;
     }
+  }
+
+  goToImage(index: number): void {
+    this.currentIndex = index;
   }
 }
